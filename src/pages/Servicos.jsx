c20e2b4053f0ab27b6bd714ece6597c9bf2a609c
RaphaelBonaccorsi <raphaelbonaccorsi@usp.br>
2025-11-18 19:39:36 -0300

    Adição do componente de serviços na pagina

--- src/pages/Servicos.jsx
+++ src/pages/Servicos.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import SectionTitle from '../components/ui/SectionTitle'
 import Hero from '../components/sections/Hero'
-
+import Services from '../components/sections/Services'
 
 export default function Servicos() {
   return (
     <>
         <Hero />
         <main className="container">
-
+          <Services />
         </main>
     </>
   )
